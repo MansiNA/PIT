292a7abb465cca0425a3c3d676933c0becbf927d
DBUSS GmbH <m.quaschny@dbuss.de>
2023-07-29 20:01:06 +0200

    HW Mapping Filter in Tabelle eingebaut

--- src/main/java/com/example/application/views/CLTV_HW_MeasuresView.java
+++ src/main/java/com/example/application/views/CLTV_HW_MeasuresView.java
@@ -7,27 +7,38 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.HeaderRow;
+import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.gridpro.GridPro;
 import com.vaadin.flow.component.gridpro.GridProVariant;
 import com.vaadin.flow.component.gridpro.ItemUpdater;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 @Route(value="HW-Mapping", layout = MainLayout.class)
 @PageTitle("HW-Mapping | TEF-Control")
 public class CLTV_HW_MeasuresView extends VerticalLayout {
 
     private final CLTV_HW_MeasureService cltvHwMeasureService;
-    GridPro<CLTV_HW_Measures> grid = new GridPro<>(CLTV_HW_Measures.class);
-    TextField filterText = new TextField();
+    //GridPro<CLTV_HW_Measures> grid = new GridPro<>(CLTV_HW_Measures.class);
+    GridPro<CLTV_HW_Measures> grid = new GridPro<>();
+
+    //Grid<CLTV_HW_Measures> grid = new Grid<>(CLTV_HW_Measures.class, false);
+
+//    TextField filterText = new TextField();
     TextField filterInt = new TextField();
 
-    ComboBox<String> comboBox = new ComboBox<>("Monat");
+  //  ComboBox<String> comboBox = new ComboBox<>("Monat");
 
 
     public CLTV_HW_MeasuresView(CLTV_HW_MeasureService cltvHwMeasureService) {
@@ -52,9 +63,16 @@ public class CLTV_HW_MeasuresView extends VerticalLayout {
     private void configureGrid() {
         grid.addClassNames("MSM-grid");
         grid.setSizeFull();
-        grid.setColumns("monat_ID", "device", "measure_Name","channel");
 
-        grid.addEditColumn(CLTV_HW_Measures::getValue)
+        //grid.setColumns("monat_ID", "device", "measure_Name","channel");
+
+        Grid.Column<CLTV_HW_Measures> MonatColumn = grid.addColumn(CLTV_HW_Measures::getMonat_ID);
+        Grid.Column<CLTV_HW_Measures> DeviceColumn = grid.addColumn(CLTV_HW_Measures::getDevice);
+        Grid.Column<CLTV_HW_Measures> MeasureColumn = grid.addColumn(CLTV_HW_Measures::getMeasure_Name);
+        Grid.Column<CLTV_HW_Measures> ChannelColumn = grid.addColumn(CLTV_HW_Measures::getChannel);
+
+
+        Grid.Column<CLTV_HW_Measures> ValueColumn =  grid.addEditColumn(CLTV_HW_Measures::getValue)
             .text(new ItemUpdater<CLTV_HW_Measures, String>(){
                 @Override
                 public void accept(CLTV_HW_Measures currow, String s) {
@@ -63,12 +81,25 @@ public class CLTV_HW_MeasuresView extends VerticalLayout {
                     cltvHwMeasureService.update(currow,s);
                     //System.out.println("Update auf" + s);
                     updateList();
-                }})
-                .setHeader("Value");
+                }});
+                //.setHeader("Value");
 
         grid.addThemeVariants(GridProVariant.LUMO_HIGHLIGHT_EDITABLE_CELLS);
 
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        List<CLTV_HW_Measures> people = cltvHwMeasureService.findAllProducts("");
+        GridListDataView<CLTV_HW_Measures> dataView = grid.setItems(people);
+        PersonFilter personFilter = new PersonFilter(dataView);
+
+        grid.getHeaderRows().clear();
+        HeaderRow headerRow = grid.appendHeaderRow();
+
+        headerRow.getCell(MonatColumn).setComponent(createFilterHeader("Monat", personFilter::setFullName));
+        headerRow.getCell(DeviceColumn).setComponent(createFilterHeader("Device", personFilter::setEmail));
+        headerRow.getCell(MeasureColumn).setComponent(createFilterHeader("Measure", personFilter::setEmail));
+        headerRow.getCell(ChannelColumn).setComponent(createFilterHeader("Channel", personFilter::setEmail));
+        headerRow.getCell(ValueColumn).setComponent(createFilterHeader("Value", personFilter::setEmail));
 
 
 //        grid.asSingleSelect().addValueChangeListener(event ->
@@ -76,26 +107,46 @@ public class CLTV_HW_MeasuresView extends VerticalLayout {
 
     }
 
+    private static Component createFilterHeader(String labelText,
+                                                Consumer<String> filterChangeConsumer) {
+        Label label = new Label(labelText);
+        label.getStyle().set("padding-top", "var(--lumo-space-m)")
+                .set("font-size", "var(--lumo-font-size-xs)");
+        TextField textField = new TextField();
+        textField.setValueChangeMode(ValueChangeMode.EAGER);
+        textField.setClearButtonVisible(true);
+        textField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+        textField.setWidthFull();
+        textField.getStyle().set("max-width", "100%");
+        textField.addValueChangeListener(
+                e -> filterChangeConsumer.accept(e.getValue()));
+        VerticalLayout layout = new VerticalLayout(label, textField);
+        layout.getThemeList().clear();
+        layout.getThemeList().add("spacing-xs");
+
+        return layout;
+    }
+
     private HorizontalLayout getToolbar() {
-        filterText.setPlaceholder("Filter by Device/Measure/Channel");
-        filterText.setClearButtonVisible(true);
-        filterText.setValueChangeMode(ValueChangeMode.LAZY);
-        filterText.addValueChangeListener(e -> updateList());
+//        filterText.setPlaceholder("Filter by Device/Measure/Channel");
+//        filterText.setClearButtonVisible(true);
+//        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+//        filterText.addValueChangeListener(e -> updateList());
 
   /*      filterInt.setPlaceholder("Filter by Monat");
         filterInt.setClearButtonVisible(true);
         filterInt.setValueChangeMode(ValueChangeMode.LAZY);
         filterInt.addValueChangeListener(e -> updateMonat());*/
 
-        comboBox.setAutoOpen(true);
-        comboBox.setItems(cltvHwMeasureService.getMonate());
-        comboBox.setLabel("");
-        //comboBox.setHelperText("Helper text");
-        comboBox.setPlaceholder("Filter by Monat");
-        comboBox.setTooltipText("Filter auf vorhandenen Monat");
-        comboBox.addValueChangeListener(e -> updateMonat());
-     //   comboBox.setClearButtonVisible(true);
-        comboBox.setPrefixComponent(VaadinIcon.SEARCH.create());
+//        comboBox.setAutoOpen(true);
+//        comboBox.setItems(cltvHwMeasureService.getMonate());
+//        comboBox.setLabel("");
+//        //comboBox.setHelperText("Helper text");
+//        comboBox.setPlaceholder("Filter by Monat");
+//        comboBox.setTooltipText("Filter auf vorhandenen Monat");
+//        comboBox.addValueChangeListener(e -> updateMonat());
+//     //   comboBox.setClearButtonVisible(true);
+//        comboBox.setPrefixComponent(VaadinIcon.SEARCH.create());
 
         Button addProductButton = new Button("Add Monat");
    //     addProductButton.addClickListener(click -> addProduct());
@@ -104,7 +155,7 @@ public class CLTV_HW_MeasuresView extends VerticalLayout {
         Button startJobButton = new Button("Start");
      //   startJobButton.addClickListener(click -> startJob());
 
-        var toolbar = new HorizontalLayout(filterText, comboBox, addProductButton, startJobButton);
+        var toolbar = new HorizontalLayout(addProductButton, startJobButton);
         toolbar.addClassName("toolbar");
 
         return toolbar;
@@ -123,13 +174,56 @@ public class CLTV_HW_MeasuresView extends VerticalLayout {
     }
     private void updateList() {
 
-        System.out.println("Suche nach String: " + filterText.getValue());
-        grid.setItems(cltvHwMeasureService.findAllProducts(filterText.getValue()));
+     //   System.out.println("Suche nach String: " + filterText.getValue());
+        //grid.setItems(cltvHwMeasureService.findAllProducts(filterText.getValue()));
     }
 
     private void updateMonat() {
 
        // System.out.println("Suche nach Monat: " + filterInt.getValue());
-        grid.setItems(cltvHwMeasureService.findProductsbyMonat(comboBox.getValue().toString()));
+      //  grid.setItems(cltvHwMeasureService.findProductsbyMonat(comboBox.getValue().toString()));
     }
+
+    private static class PersonFilter {
+        private final GridListDataView<CLTV_HW_Measures> dataView;
+
+        private String monat_ID;
+        private String email;
+        private String profession;
+
+        public PersonFilter(GridListDataView<CLTV_HW_Measures> dataView) {
+            this.dataView = dataView;
+            this.dataView.addFilter(this::test);
+        }
+
+        public void setFullName(String fullName) {
+            this.monat_ID = fullName;
+            this.dataView.refreshAll();
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+            this.dataView.refreshAll();
+        }
+
+        public void setProfession(String profession) {
+            this.profession = profession;
+            this.dataView.refreshAll();
+        }
+
+        public boolean test(CLTV_HW_Measures person) {
+            boolean matchesFullName = matches(person.getMonat_ID().toString(), monat_ID);
+            //boolean matchesEmail = matches(person.getEmail(), email);
+            //boolean matchesProfession = matches(person.getProfession(), profession);
+
+            return matchesFullName; //&& matchesEmail && matchesProfession;
+        }
+
+        private boolean matches(String value, String searchTerm) {
+            return searchTerm == null || searchTerm.isEmpty()
+                    || value.toLowerCase().contains(searchTerm.toLowerCase());
+        }
+    }
+
+
 }
